429577366413eb4f3c40a2f93e78b2e586dfe654
ShujaShah <shuja.ashraf11@gmail.com>
2023-06-01 23:48:29 +0530

    added state with counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Meme from "./components/Meme";
+import Counter from "./components/Counter";
 
 export default function App() {
   return (
     <div>
       <Header />
       <Meme />
+      <Counter />
     </div>
   );
 }
