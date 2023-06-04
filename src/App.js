bfa1fe51f99e24a397190fdd9b90fa652852339b
ShujaShah <shuja.ashraf11@gmail.com>
2023-06-04 12:24:30 +0530

    add onClick event on the box

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App() {
     <div>
       <Header />
       <Meme />
+      <Box/>
     </div>
   );
 }
--- src/components/Box.js
+++ src/components/Box.js
@@ -3,10 +3,16 @@ import boxesArr from "../boxes";
 
 const Box = () => {
   const [boxes, setBoxes] = React.useState(boxesArr);
+
+  const handleClick = (event) => {
+    const clickedBox = event.target;
+    clickedBox.style.backgroundColor = "blue";
+  };
+
   return (
     <div>
       {boxes.map((box) => (
-        <div key={box.id} className="box-div">
+        <div key={box.id} className="box-div" onClick={handleClick}>
           {box.id}
         </div>
       ))}
